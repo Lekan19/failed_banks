8ce354da9bb47b922d27700c254d129dfb803f69
Ola <gain@Ganius-MacBook-Pro.local>
2024-12-08 21:36:45 -0600

    init frontend

--- fdic-ui/js/script.js
+++ fdic-ui/js/script.js
@@ -3,73 +3,232 @@ const allBtn = document.getElementById('allDataButton');
 const stateBtn = document.getElementById('stateDataButton');
 const yearBtn = document.getElementById('yearDataButton');
 const allDataBody = document.getElementById('alldatabody');
+const yearDataBody = document.getElementById('yeardatabody');
+const stateDataBody = document.getElementById('statedatabody');
+const searchYearForm = document.getElementById("searchyearform");
+const searchStateForm = document.getElementById("searchstateform");
+const stateClearBtn = document.getElementById("clearStateDataBtn");
+const yearClearBtn = document.getElementById("clearYearDataBtn");
+const allClearBtn = document.getElementById("clearAllDataBtn")
 
-const sampleData = [{
-    "Bank_name": "UBA",
-    "city": "Ikeja", 
-    "state": "Lagos",
-    "cert": 123,
-    "Acquisition": "BOA",
-    "Date": "23Nov2024",
-    "Fund": 12
-}, 
-{
-    "Bank_name": "UBA",
-    "city": "Ikeja", 
-    "state": "Lagos",
-    "cert": 123,
-    "Acquisition": "BOA",
-    "Date": "23Nov2024",
-    "Fund": 12
-}]
 
-function allData(){
+const apiUrl_allData = "http://127.0.0.1:9000/api/failed_banks"; 
 
-    console.log(sampleData)
+async function fetchYearData(year){
+    console.log(year)
+    if (year){
+
+    
+    try {
+        const response = await fetch(`http://127.0.0.1:9000/api/${year}`); // Fetch data from FastAPI
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const yearDataJson = await response.json(); // Parse JSON response
+        yearDataFunc(yearDataJson); // Render the data to the UI
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }}
+    else{
+        alert("Enter year number")
+    }
+}
+
+async function fetchStateData(state_id){
+    console.log(state_id)
+    if (state_id){
+    
+    try {
+        const response = await fetch(`http://127.0.0.1:9000/api/failed_banks/${state_id}`); // Fetch data from FastAPI
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        
+       
+        const stateDataJson = await response.json(); // Parse JSON response
+        stateDataFunc(stateDataJson); // Render the data to the UI
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+    }
+    
+    else {alert("Enter state id")}
+}
+async function fetch_allData() {
+    try {
+        const response = await fetch(apiUrl_allData); // Fetch data from FastAPI
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const sampleData = await response.json(); // Parse JSON response
+        allData(sampleData); // Render the data to the UI
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+function allData(apiData){
+
+    console.log(apiData)
 
     // Populate the table with rows
-    sampleData.forEach(item => {
+    apiData.forEach(item => {
         const row = document.createElement("tr");
         allDataBody.appendChild(row);
 
         // Create and append cells
         const banknameCell = document.createElement("td");
-        banknameCell.textContent = item.Bank_name;
+        banknameCell.textContent = item['Bank name'];
         row.appendChild(banknameCell);
 
         const cityCell = document.createElement("td");
-        cityCell.textContent = item.city;
+        cityCell.textContent = item.City;
         row.appendChild(cityCell);
 
         const stateCell = document.createElement("td");
-        stateCell.textContent = item.state;
+        stateCell.textContent = item.State;
         row.appendChild(stateCell);
 
         const certCell = document.createElement("td");
-        certCell.textContent = item.cert;
+        certCell.textContent = item.Cert;
         row.appendChild(certCell);
 
         const acquisitionCell = document.createElement("td");
-        acquisitionCell.textContent = item.Acquisition;
+        acquisitionCell.textContent = item["Acquisition institution"];
         row.appendChild(acquisitionCell);
 
         const dateCell = document.createElement("td");
-        dateCell.textContent = item.Date;
+        dateCell.textContent = item["Closing date"];
         row.appendChild(dateCell);
 
         const fundCell = document.createElement("td");
         fundCell.textContent = item.Fund;
         row.appendChild(fundCell);
 
+        
+    });
+   
+}
 
+function yearDataFunc(apiData){
+
+    console.log(apiData)
+
+    // Populate the table with rows
+    apiData.forEach(item => {
+        const row = document.createElement("tr");
+        yearDataBody.appendChild(row);
+
+        // Create and append cells
+        const banknameCell = document.createElement("td");
+        banknameCell.textContent = item['Bank name'];
+        row.appendChild(banknameCell);
+
+        const cityCell = document.createElement("td");
+        cityCell.textContent = item.City;
+        row.appendChild(cityCell);
+
+        const stateCell = document.createElement("td");
+        stateCell.textContent = item.State;
+        row.appendChild(stateCell);
+
+        const certCell = document.createElement("td");
+        certCell.textContent = item.Cert;
+        row.appendChild(certCell);
+
+        const acquisitionCell = document.createElement("td");
+        acquisitionCell.textContent = item["Acquisition institution"];
+        row.appendChild(acquisitionCell);
+
+        const dateCell = document.createElement("td");
+        dateCell.textContent = item["Closing date"];
+        row.appendChild(dateCell);
+
+        const fundCell = document.createElement("td");
+        fundCell.textContent = item.Fund;
+        row.appendChild(fundCell);
 
         
     });
    
 }
 
-allBtn.addEventListener('click', allData);
-function stateData(){}
-function yearData(){}
-// stateBtn.addEventListener('click', stateData);
-// yearBtn.addEventListener('click', yearData);
+function stateDataFunc(apiData){
+
+    console.log(apiData)
+
+    // Populate the table with rows
+    apiData.forEach(item => {
+        const row = document.createElement("tr");
+        stateDataBody.appendChild(row);
+
+        // Create and append cells
+        const banknameCell = document.createElement("td");
+        banknameCell.textContent = item['Bank name'];
+        row.appendChild(banknameCell);
+
+        const cityCell = document.createElement("td");
+        cityCell.textContent = item.City;
+        row.appendChild(cityCell);
+
+        const stateCell = document.createElement("td");
+        stateCell.textContent = item.State;
+        row.appendChild(stateCell);
+
+        const certCell = document.createElement("td");
+        certCell.textContent = item.Cert;
+        row.appendChild(certCell);
+
+        const acquisitionCell = document.createElement("td");
+        acquisitionCell.textContent = item["Acquisition institution"];
+        row.appendChild(acquisitionCell);
+
+        const dateCell = document.createElement("td");
+        dateCell.textContent = item["Closing date"];
+        row.appendChild(dateCell);
+
+        const fundCell = document.createElement("td");
+        fundCell.textContent = item.Fund;
+        row.appendChild(fundCell);
+
+        
+    });
+   
+}
+
+function clearAllTable(){
+    allDataBody.innerHTML = ""; // Remove all rows from the table body
+
+}
+
+function clearYearTable(){
+    yearDataBody.innerHTML = ""; // Remove all rows from the table body
+
+}
+
+function clearStateTable(){
+    stateDataBody.innerHTML = ""; // Remove all rows from the table body
+
+}
+
+// EVENT LISTERNERS 
+
+allBtn.addEventListener('click', fetch_allData);
+// searchForm.addEventListener("click", fetchYearData);
+
+searchYearForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent the default form submission
+    const yearInput = document.getElementById("yearInput").value;
+    fetchYearData(yearInput); // Fetch and display data for the entered year
+});
+searchStateForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent the default form submission
+    const stateInput = document.getElementById("stateInput").value;
+    fetchStateData(stateInput); // Fetch and display data for the entered year
+});
+
+// clearing row data event listener
+
+yearClearBtn.addEventListener("click", clearYearTable)
+stateClearBtn.addEventListener("click", clearStateTable)
+allClearBtn.addEventListener("click", clearAllTable)
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from query_db import get_all_banks, get_state_data, get_year_data
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins. Use specific origins in production.
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods.
+    allow_headers=["*"],  # Allows all headers.
+)
 
 @app.get("/api/failed_banks")
 async def get_banks():
